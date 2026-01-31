cabd00331bf7bea77d33ad59aa7c6ec3450c58da
Rhiannon Barbero <barberor@msu.edu>
2026-01-31 13:27:48 -0500

    student button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
       <h1>Hello Amelia!</h1>
+      <Link href="/student-signup">
+        <button>Student Sign Up</button>
+      </Link>
     </div>
   )
 }
